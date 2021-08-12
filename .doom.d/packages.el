51b4570b45718fbd456eac63abbb2f7374138dc6
ning <ningyuan.sg@gmail.com>
2021-08-13 01:43:25 +0800

    Add org-roam-ui

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -9,6 +9,10 @@
 ;(package! some-package)
 (package! org-download)
 (package! snakemake-mode)
+(package! org-noter)
+; org-roam-ui dependencies
+(package! websocket)
+(package! simple-httpd)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
@@ -52,3 +56,4 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 (unpin! org-roam company-org-roam)
+(package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
