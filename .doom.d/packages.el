400338598ca3918a1b55881a5e677985aadaff87
Ning Yuan <ningyuan.sg@gmail.com>
2021-08-21 12:46:05 +0800

    Add page-break-lines.el

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -13,6 +13,8 @@
 ; org-roam-ui dependencies
 (package! websocket)
 (package! simple-httpd)
+; etc.
+(package! page-break-lines :recipe (:host github :repo "purcell/page-break-lines"))
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
