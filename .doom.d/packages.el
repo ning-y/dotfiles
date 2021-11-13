ce606c11b15450bbc86745a211c1722c91e9482b
ning <ningyuan.sg@gmail.com>
2021-11-13 23:46:03 +0800

    Pin org-mode to 9.5

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -50,6 +50,7 @@
 
 ;; Use `:pin' to specify a particular commit to install.
 ;(package! builtin-package :pin "1a2b3c4d5e")
+(package! org-mode :pin "7387593")  ; pin at 9.5
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
